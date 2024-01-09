dd5a1c8e7f7d77a4732f0a4ff9a3340d693f1887
Adam Gonzales <adamgonzales1@gmail.com>
2024-01-09 14:27:41 -0700

    config: add .nvmrc to fix node version too low issue on local dev - update pdf resume to most recent version - move header javascript from Header.astro to navToggle.js - update content on style.astro

---
+++ src/js/navToggle.js
@@ -0,0 +1,16 @@
+const navToggle = document.querySelector('.nav-toggle')
+const navLinks = document.querySelectorAll('.nav-items li')
+const pageContainer = document.querySelector('.page-container')
+const mainNav = document.querySelector('.main-nav')
+
+window.addEventListener('load', (event) => {
+  mainNav.classList.remove('preload')
+})
+navToggle.addEventListener('click', () => {
+  pageContainer.classList.toggle('nav-open')
+})
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    pageContainer.classList.remove('nav-open')
+  })
+})
