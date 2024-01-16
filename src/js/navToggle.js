bd8806d22a0865548b7f00b422c1a8802d8d1da9
Adam Gonzales <adamgonzales1@gmail.com>
2024-01-16 11:21:41 -0700

    refactor: move navToggle code into it's own file - update content in intro section, head, and style page - add conditional for updatedDate in BlogSingle - some CSS

--- src/js/navToggle.js
+++ src/js/navToggle.js
@@ -3,14 +3,16 @@ const navLinks = document.querySelectorAll('.nav-items li')
 const pageContainer = document.querySelector('.page-container')
 const mainNav = document.querySelector('.main-nav')
 
-window.addEventListener('load', (event) => {
-  mainNav.classList.remove('preload')
-})
-navToggle.addEventListener('click', () => {
-  pageContainer.classList.toggle('nav-open')
-})
-navLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    pageContainer.classList.remove('nav-open')
+document.addEventListener('astro:page-load', () => {
+  window.addEventListener('load', (event) => {
+    mainNav.classList.remove('preload')
+  })
+  navToggle.addEventListener('click', () => {
+    pageContainer.classList.toggle('nav-open')
+  })
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      pageContainer.classList.remove('nav-open')
+    })
   })
 })
