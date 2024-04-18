bed60840fa662ccc036f47ed00a4ff67eaecbc89
Adam Gonzales <adamgonzales1@gmail.com>
2024-04-18 11:02:28 -0600

    config: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 import mdx from "@astrojs/mdx"
 import partytown from "@astrojs/partytown"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://codedbyadam.com",
   integrations: [
     react(),
     mdx(),
+    sitemap(),
     partytown({
       config: {
         forward: ["dataLayer.push"],
